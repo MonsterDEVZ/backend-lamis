c74387446cb0e4de085812a87b0b3e6e051f5ef7
AnKomsuu <udzumakin633445@gmail.com>
2025-11-29 19:13:39 +0600

    Allow text-only partners

--- apps/partners/models.py
+++ apps/partners/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Partner(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название")
-    logo = models.ImageField(upload_to='partners_logos/', verbose_name="Логотип")
+    logo = models.ImageField(upload_to='partners_logos/', verbose_name="Логотип", blank=True, null=True)
 
     class Meta:
         verbose_name = "Клиент"
